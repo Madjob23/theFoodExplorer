c5696859c3659322fa01ccab5204c9f4f7adbeb6
madhav <smadhvendra20@gmail.com>
2025-04-07 01:59:04 +0530

    Add cart reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/products/productSlice';
-import cartReducer from '../features/cart/cartSlice';
+import cartReducer, { saveCartToStorage } from '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     cart: cartReducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat((store) => (next) => (action) => {
+      const result = next(action);
+      if (action.type?.startsWith('cart/')) {
+        saveCartToStorage(store.getState().cart);
+      }
+      return result;
+    })
 });
